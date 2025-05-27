1788aacd30a1b40678d324a35e4e9a5913c39e7e
Chinweike99 <innocentakwolu1@gmail.com>
2025-05-27 18:10:10 +0100

    testing lint

--- src/controllers/wallet.js
+++ src/controllers/wallet.js
@@ -1,3 +1,12 @@
 export const createWallet = () => {
     return console.log("Create wallet: ")
 }
+
+export const createdWallet = () => {
+  const user = 'John';
+  console.log('Create wallet');
+};
+
+export const createWalletTest = () => {
+  return 'wallet created'
+}
