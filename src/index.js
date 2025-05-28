e31f0ec8b375600fb27d5384a941d179a8b09c5f
Zinhle Nhlapo <zinhle.nhlapo31@gmail.com>
2025-05-28 23:07:28 +0200

    Added a route.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv'
-import { createWallet } from './controllers/wallet.js';
+import walletRoute from './routes/walletRoute.js';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000
 
-createWallet();
+app.use(express.json());
+
+app.use('/api/wallet', walletRoute);
+
+
 app.listen(port, ()=> {
     console.log(`Server running on port: ${port}`)
 })
