341e1415f2803da5821eb21c011eac291dbfd136
= <=>
2025-05-28 06:05:36 +0100

    adds cors and other basic security middlewares

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import express from 'express';
+import express, { urlencoded, json } from 'express';
+import hpp from 'hpp';
+import helmet from 'helmet';
+import cors from 'cors';
+import compression from 'compression';
 import { createWallet } from './controllers/wallet.js';
 import { config } from './configs/config.env.js';
 
 const app = express();
-const SERVER_PORT = config.SERVER_PORT;
+
+const SERVER_PORT = config.SERVER_PORT || 4000;
+
+app.use(hpp());
+app.use(helmet());
+app.use(
+  cors({
+    origin: config.CLIENT_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  })
+);
+app.use(compression());
+app.use(json({ limit: '200mb' }));
+app.use(urlencoded({ extended: true, limit: '200mb' }));
 
 createWallet(); // Example usage of createWallet function
+
+
+
+
 app.listen(SERVER_PORT, ()=> {
     console.log(`Server running on port: ${SERVER_PORT}`)
 })
